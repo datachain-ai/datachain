863c8c11b73592b87047e03426277f06a7250ae8
Ivan Shcheklein <shcheklein@gmail.com>
2026-03-24 18:52:35 -0700

    print package versions

--- noxfile.py
+++ noxfile.py
@@ -16,6 +16,10 @@ python_versions = nox.project.python_versions(project)
 locations = "src", "tests"
 
 
+def log_installed_packages(session: nox.Session) -> None:
+    session.run("uv", "pip", "list")
+
+
 @nox.session
 def docs(session: nox.Session) -> None:
     session.install(".[docs]")
@@ -25,6 +29,7 @@ def docs(session: nox.Session) -> None:
 @nox.session
 def bench(session: nox.Session) -> None:
     session.install(".[tests]")
+    log_installed_packages(session)
     session.run(
         "pytest",
         "--benchmark-only",
@@ -37,6 +42,7 @@ def bench(session: nox.Session) -> None:
 @nox.session(python=python_versions)
 def tests(session: nox.Session) -> None:
     session.install(".[tests]")
+    log_installed_packages(session)
     env = {"COVERAGE_FILE": f".coverage.{session.python}"}
     if session.python in ("3.12", "3.13"):
         # improve performance of tests in Python>=3.12 when used with coverage
@@ -59,6 +65,7 @@ def tests(session: nox.Session) -> None:
 @nox.session(python=python_versions)
 def e2e(session: nox.Session) -> None:
     session.install(".[tests]")
+    log_installed_packages(session)
     session.run(
         "pytest",
         "--durations=0",
@@ -90,7 +97,7 @@ def build(session: nox.Session) -> None:
 @nox.session(python=python_versions)
 def examples(session: nox.Session) -> None:
     session.install(".[examples]")
-    session.run("uv", "pip", "list")
+    log_installed_packages(session)
     session.run(
         "pytest",
         "--durations=0",
